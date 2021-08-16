8a27e1462e7b89e9dbf690e09c502e28839e8f66
Jesse Larrew <jesse.larrew@amd.com>
2021-08-16 22:40:11 +0000

    sev-guest: initial implementation
    Design: sev-guest is a wrapper program that takes commands and
    invokes additional programs to execute those commands.
    
    Q: Does this need that level of separation between the main tool
    and the sub commands? Could the commands just be functions?
    
    Signed-off-by: Jesse Larrew <jesse.larrew@amd.com>

---
+++ src/sev-guest.c
@@ -0,0 +1,77 @@
+/* Copyright (C) 2021 Advanced Micro Devices, Inc. */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <linux/sev-guest.h>
+#include <linux/psp-sev.h>
+
+#ifndef PROG_NAME
+#define PROG_NAME	"sev-guest"
+#endif
+
+#define MIN_NR_ARGS	(2)
+#define CMD_INDEX	(1)
+
+#define ARRAY_SIZE(x)	(sizeof(x)/sizeof(x[0]))
+
+void print_usage(void)
+{
+	fprintf(stderr, "Usage: " PROG_NAME " command [args...]\n"
+		"\n"
+		"commands:\n"
+		"    get-report: retrieve the attestation report from \n"
+		"                the firmware.\n"
+		"\n");
+}
+
+int main(int argc, char *argv[])
+{
+	int rc = EXIT_FAILURE;
+	const char *command = NULL;
+
+	struct entry {
+		const char *command;
+		char *exec_name;
+	} commands[] = {
+		{ "get-report", PROG_NAME "-get-report" },
+	};
+
+	if (argc < MIN_NR_ARGS) {
+		fprintf(stderr, PROG_NAME ": no command specified.\n\n");
+		print_usage();
+		rc = -EINVAL;
+		goto exit;
+	}
+
+	command = argv[CMD_INDEX];
+
+	for (size_t i = 0; i < ARRAY_SIZE(commands); i++) {
+		/*
+		 * strlen() is safe here, because the command name is
+		 * initialized from a static string that is guaranteed to be
+		 * null-terminated.
+		 */
+		if (!strncmp(command, commands[i].command, strlen(commands[i].command))) {
+			char **args = argv + CMD_INDEX;
+			args[0] = commands[i].exec_name;
+			errno = 0;
+			execvp(commands[i].exec_name, args);
+
+			/* If exec returned, then an error occured */
+			rc = errno;
+			perror("execlp");
+			goto exit;
+		}
+	}
+
+	/* If we made it here, then the requested command is not supported */
+	rc = ENOTSUP;
+	fprintf(stderr, PROG_NAME ": unsupported command.\n");
+	print_usage();
+exit:
+	exit(rc);
+}
+
