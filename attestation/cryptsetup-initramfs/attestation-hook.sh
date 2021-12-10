a118565e2299ab790016ccb016d5f58dff74dbef
Jesse Larrew <jesse.larrew@amd.com>
2021-12-10 20:29:26 +0000

    cryptsetup-initramfs: encrypt disk key
    The disk encryption key is secret and must be protected during
    transit. To accomplish this, the guest will generate a keypair
    and include the fingerprint (hash) of the public key in the
    attestation report. Then, the public key and the attestation
    report are sent together to the guest owner.
    
    The attestation report proves that the guest that generated the
    report also owns the public key sent with the report. After
    validating the report, the guest owner can use the public key to
    encrypt the disk key before sending it to the guest.
    
    Finally, the guest can use the private key to decrypt the disk key.
    
    In addition to assuring confidentiality of the disk key, this also
    mitigates MITM attacks where an attacker captures the attestation
    report and replays it to the guest owner to try to expose the key.
    
    Signed-off-by: Jesse Larrew <jesse.larrew@amd.com>

--- attestation/cryptsetup-initramfs/attestation-hook.sh
+++ attestation/cryptsetup-initramfs/attestation-hook.sh
@@ -24,8 +24,13 @@ copy_file firmware /lib/firmware/amd/sev.fw
 # Add the userspace tool to request the attestation report
 copy_exec /usr/bin/sev-guest-get-report
 
-# Add curl to send the report to our attestation server
+# Add tar and curl to send the report to our attestation server
+copy_exec /usr/bin/tar
 copy_exec /usr/bin/curl
 
 # Add the CA root certificate for our attestation server
 copy_file PEM /usr/local/share/ca-certificates/jlarrew-root-ca-cert.crt
+
+# Add the local build of openssl3
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib64/
+copy_exec /usr/local/bin/openssl
