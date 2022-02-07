893146e69d47ed383b08d53b20f32b266cf3b001
Jesse Larrew <jesse.larrew@amd.com>
2022-02-07 22:46:35 +0000

    cryptsetup-initramfs: fix potential ordering issue
    Since sev-guest-get-report and openssl both require the openssl 3.0
    libraries, they should both be added to the initrd after LD_LIBRARY_PATH
    has been updated with the location of the openssl 3.0 libraries.
    
    Signed-off-by: Jesse Larrew <jesse.larrew@amd.com>

--- attestation/cryptsetup-initramfs/attestation-hook.sh
+++ attestation/cryptsetup-initramfs/attestation-hook.sh
@@ -20,9 +20,6 @@ esac
 # Add the CCP module for retrieving the attestation report
 manual_add_modules ccp
 
-# Add the userspace tool to request the attestation report
-copy_exec /usr/bin/sev-guest-get-report
-
 # Add tar and curl to send the report to our attestation server
 copy_exec /usr/bin/tar
 copy_exec /usr/bin/curl
@@ -33,3 +30,6 @@ copy_exec /usr/bin/curl
 # Add the local build of openssl3
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib64/
 copy_exec /usr/local/bin/openssl
+
+# Add the userspace tool to request the attestation report
+copy_exec /usr/bin/sev-guest-get-report
